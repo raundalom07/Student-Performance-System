77b5077da6ce90091a09d00c23666c3837f9c834
Om Raundal <raundalom856@gmail.com>
2026-02-13 19:55:20 +0530

    Day 1 completed - backend setup and health API

---
+++ backend/src/main/java/com/om/backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.om.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Student Performance Backend Running";
+    }
+}
