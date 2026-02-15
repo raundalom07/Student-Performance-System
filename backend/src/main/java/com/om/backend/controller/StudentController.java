e3951cf0130ee7157171e613dbd7f0cf2e075fde
Om Raundal <raundalom856@gmail.com>
2026-02-15 23:07:06 +0530

    Day 3 completed - Student CRUD APIs implemented

---
+++ backend/src/main/java/com/om/backend/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.om.backend.controller;
+
+import com.om.backend.model.Student;
+import com.om.backend.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@CrossOrigin
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @PostMapping
+    public Student saveStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
