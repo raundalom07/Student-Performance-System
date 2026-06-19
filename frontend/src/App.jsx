24052424c66b6820fab6e9d4fb0787267314c4e4
Om Raundal <raundalom856@gmail.com>
2026-06-19 22:54:55 +0530

    Frontend Day 1 completed - React setup and project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Student Performance Prediction System</h1>
+      <h3>Frontend Day 1 Setup Complete 🚀</h3>
+    </div>
+  );
+}
+
+export default App;
